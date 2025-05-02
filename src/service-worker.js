db1ad867541bd481519419c2189bd9a2842dda44
Pavel Biryuchev <pavel_biryuchev@inbox.ru>
2025-05-02 17:29:32 +0500

    feat: map tiles caching

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,5 +1,3 @@
-/* eslint-disable no-restricted-globals */
-
 import { clientsClaim } from 'workbox-core';
 import { ExpirationPlugin } from 'workbox-expiration';
 import { precacheAndRoute, createHandlerBoundToURL } from 'workbox-precaching';
@@ -8,35 +6,45 @@ import { StaleWhileRevalidate } from 'workbox-strategies';
 
 clientsClaim();
 
-precacheAndRoute(self.__WB_MANIFEST);
-
-const fileExtensionRegexp = new RegExp('/[^/?]+\\.[^/]+$');
-registerRoute(({ request, url }) => {
-  if (request.mode !== 'navigate') {
-    return false;
-  }
-  if (url.pathname.startsWith('/_')) {
-    return false;
-  }
-
-  if (url.pathname.match(fileExtensionRegexp)) {
-    return false;
-  }
-
-  return true;
-}, createHandlerBoundToURL(process.env.PUBLIC_URL + '/index.html'));
-
 registerRoute(
   ({ url }) =>
-    url.origin === self.location.origin && url.pathname.endsWith('.png'),
+    url.pathname.includes('/styles/v1/') && url.pathname.includes('/tiles/'),
   new StaleWhileRevalidate({
-    cacheName: 'images',
-    plugins: [new ExpirationPlugin({ maxEntries: 50 })],
+    cacheName: 'map-tiles',
+    plugins: [
+      new ExpirationPlugin({
+        maxEntries: 500,
+        maxAgeSeconds: 30 * 24 * 60 * 60,
+      }),
+    ],
   })
 );
 
+precacheAndRoute(self.__WB_MANIFEST);
+
+registerRoute(
+  ({ request }) => request.destination === 'image',
+  new StaleWhileRevalidate({
+    cacheName: 'images',
+    plugins: [
+      new ExpirationPlugin({
+        maxEntries: 100,
+        maxAgeSeconds: 7 * 24 * 60 * 60,
+      }),
+    ],
+  })
+);
+
+const fileExtensionRegexp = new RegExp('/[^/?]+\\.[^/]+$');
+registerRoute(({ request, url }) => {
+  if (request.mode !== 'navigate') return false;
+  if (url.pathname.startsWith('/_')) return false;
+  if (url.pathname.match(fileExtensionRegexp)) return false;
+  return true;
+}, createHandlerBoundToURL(process.env.PUBLIC_URL + '/index.html'));
+
 self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
+  if (event.data?.type === 'SKIP_WAITING') {
     self.skipWaiting();
   }
 });
